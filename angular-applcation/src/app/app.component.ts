558a3adcd0c21ff25ae602614dde012dc1161df4
Darien-Cepani <darien.cepani42@gmail.com>
2024-03-13 14:05:05 +0200

    First Commit, Did angular app, doing React.

---
+++ angular-applcation/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'final';
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  submit(): void {
+    if (this.myForm.valid) console.log(this.myForm.value);
+    else console.log("Invalid input");
+  }
+}
